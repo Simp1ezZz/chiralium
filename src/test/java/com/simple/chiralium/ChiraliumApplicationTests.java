4c10bac26b2cdd8b77ac50d56c8bf7fde1d0f7d0
simplezzz <2466714892@qq.com>
2024-06-30 17:52:26 +0800

    feat: 🎉 初始化提交

---
+++ src/test/java/com/simple/chiralium/ChiraliumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simple.chiralium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChiraliumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

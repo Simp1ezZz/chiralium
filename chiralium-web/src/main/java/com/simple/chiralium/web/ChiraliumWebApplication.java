8bb2c8a9069d436f1bdca02fd35616b37b1553bb
simplezzz <2466714892@qq.com>
2024-06-30 19:27:39 +0800

    feat: 🎉 新建模块包

---
+++ chiralium-web/src/main/java/com/simple/chiralium/web/ChiraliumWebApplication.java
@@ -0,0 +1,13 @@
+package com.simple.chiralium.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChiraliumWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChiraliumWebApplication.class, args);
+    }
+
+}
--- chiralium-web/src/test/java/com/simple/chiralium/web/ChiraliumWebApplicationTests.java
+++ chiralium-web/src/test/java/com/simple/chiralium/web/ChiraliumWebApplicationTests.java
@@ -1,13 +1,13 @@
-package com.simple.chiralium;
+package com.simple.chiralium.web;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ChiraliumApplicationTests {
+class ChiraliumWebApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
